c959d23e1b93021d406de3232e4cd191e90da3ec
marioharo <98289007+marioharo@users.noreply.github.com>
2022-05-22 21:00:09 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
-// Funcion para que el nav menu tenga fondo negro al hacer scroll
+//+++++++ Funcion para que el nav menu tenga fondo negro al hacer scroll +++++++//
 $(document).scroll( function(e){
   const y = $("html").scrollTop();
   if(y > 900) $("nav").addClass("nav-black")
   else $("nav").removeClass("nav-black")
 })
 
+//+++++++++ Boton go to top ++++++++++//
+mybutton = document.getElementById("btn_gototop");
+window.onscroll = function() {scrollFunction()};
+// Cuando se hace scroll hacia abajo a los 600px el botón aparece
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 600) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// Función para ir hacia arriba suavemente
+var scrollToTopBtn = document.getElementById("btn_gototop");
+var rootElement = document.documentElement;
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+btn_gototop.addEventListener("click", scrollToTop);
